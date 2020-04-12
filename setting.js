7599cf2418406be98b7786f477dc908c073c567d
Hong Anh <48637725+leanh29@users.noreply.github.com>
2020-04-12 13:09:18 +0700

    12/4

--- setting.js
+++ setting.js
@@ -1,7 +1,7 @@
 exports.dbConfig = {
-    user:"sa",
-    password: "0909",
-    server: "localhost",
-    database: "BOOKSTORE",
+    user:"DB_A57E8F_honganh_admin",
+    password: "leanhbb2909",
+    server: "sql5047.site4now.net",
+    database: "DB_A57E8F_honganh",
     port: 1433
 };
