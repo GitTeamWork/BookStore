21a814b80a6a3dd2016762df03f443326871b551
My Team <63581911+GitTeamWork@users.noreply.github.com>
2020-04-23 21:33:09 +0700

    delete

--- public/js/admin/product-admin.js
+++ public/js/admin/product-admin.js
@@ -17,35 +17,55 @@
 //     $test.html(str)
 // });
 
-let $test = $('#ShowProduct')
+let $test = $("#ShowProduct");
 var settings = {
-    "async": true,
-    "url": "/api/productList",
-    "method": "GET",
-    "headers": {
-      "cache-control": "no-cache",
-    },
-  }
-  
+  async: true,
+  url: "/api/productList",
+  method: "GET",
+  headers: {
+    "cache-control": "no-cache",
+  },
+};
+
+const LoadProduct = () => {
   $.ajax(settings).done(function (response) {
     console.log(response);
-    let str = '';
-        response.map(function(item){
-            str +=
-                 `<tr>
-                        <td>${item.productId}</td>
-                        <td>${item.productName}</td>
-                        <td>${item.price}</td>
-                        <td><img style="width=100px;height:150px" src="${item.image}"></td>
-                        <td>${item.detail}</td>
-                        <td>${item.inventory}</td>
-                        <td>${item.catalogId}</td>
-                        <td>${item.publisherId}</td>
-                        <td>Delete</td>
-                        <td>Update</td>
-                        <td>Add</td>
-                 </tr>
-                 `
-         })
-     $test.html(str)
+    let str = "";
+    response.map(function (item) {
+      str += `<tr>
+                          <td>${item.productId}</td>
+                          <td>${item.productName}</td>
+                          <td>${item.price}</td>
+                          <td><img style="width=100px;height:150px" src="${item.image}"></td>
+                          <td>${item.detail}</td>
+                          <td>${item.inventory}</td>
+                          <td>${item.catalogId}</td>
+                          <td>${item.publisherId}</td>
+                          <td><button id="delete" onclick="deleteProduct(${item.productId})" >Delete</button></td>
+                          <td><button id="update">Update</button></td>
+                          <td><button id ="add" >Add</button></td>
+                   </tr>
+                   `;
+    });
+    $test.html(str);
   });
+};
+LoadProduct();
+const deleteProduct = (idProduct) => {
+  if (confirm(`Are you sure delete product id : ${idProduct}`)) {
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url: `http://localhost:9000/api/delProduct/${idProduct}`,
+      method: "DELETE",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+    };
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      LoadProduct();
+    });
+  }
+};
