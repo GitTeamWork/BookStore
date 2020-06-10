bf1d3fad4f29735f810aac617dbf09e4864d3c4e
My Team <63581911+GitTeamWork@users.noreply.github.com>
2020-06-10 22:27:49 +0700

    user

--- public/js/admin/user-admin.js
+++ public/js/admin/user-admin.js
@@ -104,7 +104,7 @@ $("#form-addpublisher").submit((e) => {
 
 
 let $getproduct = $("#str");
-const LoadUser = () => {
+const LoadgetUser = () => {
   var full_url = document.URL; // Get current url
   var url_array = full_url.split('/') // Split the string into an array with / as separator
   var last_segment = url_array[url_array.length - 1];  // Get the last part of the array (-1)
@@ -155,7 +155,7 @@ const LoadUser = () => {
   });
 };
 
-LoadUser();
+LoadgetUser();
 
 
 
